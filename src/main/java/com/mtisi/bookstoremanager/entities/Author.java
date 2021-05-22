311155262582a9af875a09d09fec9552e73d94bd
Rafael Missio <tmissio@hotmail.com>
2021-05-22 07:09:28 -0300

    created the author entities and books

---
+++ src/main/java/com/mtisi/bookstoremanager/entities/Author.java
@@ -0,0 +1,18 @@
+package com.mtisi.bookstoremanager.entities;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
